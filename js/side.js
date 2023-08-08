a9f4bd08414151b264ad515847bc1a6f0ca2fcbe
geunhaemoon <nevve1@duam.net>
2023-08-08 16:45:18 +0900

    feat :  todolist

---
+++ js/side.js
@@ -0,0 +1,4 @@
+const sidemenuToggleButtonOnClickHandle = () => {
+    const subMenu = document.querySelector(".sub-menu");
+    const sidemenuToggleButton = document.querySelector(".menu-btn todolist");
+}
